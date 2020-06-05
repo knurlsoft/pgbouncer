a52b635bbcec574bc799260b0f383c733c18e03f
coreyleavitt <cleavitt@24-m.com>
2020-06-05 16:29:48 -0500

    Dynamic build
    - switch to multi-stage docker build to improve size
    - update entrypoint.sh so read password from file `docker inspect` will not show plaintext password

--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,29 @@
-FROM alpine:3.11
-ARG VERSION=1.12.0
+ARG ALPINE_VERSION
+FROM alpine:${ALPINE_VERSION} AS target
 
-# Inspiration from https://github.com/gmr/alpine-pgbouncer/blob/master/Dockerfile
-RUN \
-  # Download
-  apk --update add autoconf autoconf-doc automake udns udns-dev curl gcc libc-dev libevent libevent-dev libtool make man openssl-dev pkgconfig postgresql-client && \
-  curl -o  /tmp/pgbouncer-$VERSION.tar.gz -L https://pgbouncer.github.io/downloads/files/$VERSION/pgbouncer-$VERSION.tar.gz && \
-  cd /tmp && \
-  # Unpack, compile
-  tar xvfz /tmp/pgbouncer-$VERSION.tar.gz && \
-  cd pgbouncer-$VERSION && \
-  ./configure --prefix=/usr --with-udns && \
-  make && \
-  # Manual install
-  cp pgbouncer /usr/bin && \
-  mkdir -p /etc/pgbouncer /var/log/pgbouncer /var/run/pgbouncer && \
-  # entrypoint installs the configuation, allow to write as postgres user
-  cp etc/pgbouncer.ini /etc/pgbouncer/pgbouncer.ini.example && \
-  cp etc/userlist.txt /etc/pgbouncer/userlist.txt.example && \
-  addgroup -g 70 -S postgres 2>/dev/null && \
-  adduser -u 70 -S -D -H -h /var/lib/postgresql -g "Postgres user" -s /bin/sh -G postgres postgres 2>/dev/null && \
-  chown -R postgres /var/run/pgbouncer /etc/pgbouncer && \
-  # Cleanup
-  cd /tmp && \
-  rm -rf /tmp/pgbouncer*  && \
-  apk del --purge autoconf autoconf-doc automake udns-dev curl gcc libc-dev libevent-dev libtool make man libressl-dev pkgconfig
-ADD entrypoint.sh /entrypoint.sh
-USER postgres
-EXPOSE 5432
-ENTRYPOINT ["/entrypoint.sh"]
-CMD ["/usr/bin/pgbouncer", "/etc/pgbouncer/pgbouncer.ini"]
+FROM target as build_stage
+ARG PGBOUNCER_VERSION
+
+RUN apk --update add \
+    build-base \
+    libevent-dev \
+    openssl-dev \
+    c-ares-dev
+
+WORKDIR /src
+ADD https://www.pgbouncer.org/downloads/files/${PGBOUNCER_VERSION}/pgbouncer-${PGBOUNCER_VERSION}.tar.gz pgbouncer.tar.gz
+RUN tar -xf pgbouncer.tar.gz --strip 1
+RUN ./configure --prefix=/pgbouncer
+RUN make -j$(nproc)
+RUN make install
+
+FROM target
+RUN apk --update add \
+    libevent \
+    openssl \
+    c-ares
+
+WORKDIR /
+COPY --from=build_stage /pgbouncer/bin/. /usr/bin
+COPY entrypoint.sh ./
+ENTRYPOINT ["./entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,149 +1,113 @@
 #!/bin/sh
-# Based on https://raw.githubusercontent.com/brainsam/pgbouncer/master/entrypoint.sh
 
-set -e
-
-# Here are some parameters. See all on
-# https://pgbouncer.github.io/config.html
-
+PG_LOG=${LOGFILE:-/var/log/pgbouncer}
 PG_CONFIG_DIR=/etc/pgbouncer
-
-if [ -n "$DATABASE_URL" ]; then
-  # Thanks to https://stackoverflow.com/a/17287984/146289
-
-  # Allow to pass values like dj-database-url / django-environ accept
-  proto="$(echo $DATABASE_URL | grep :// | sed -e's,^\(.*://\).*,\1,g')"
-  url="$(echo $DATABASE_URL | sed -e s,$proto,,g)"
-
-  # extract the user and password (if any)
-  userpass=$(echo $url | grep @ | sed -r 's/^(.*)@([^@]*)$/\1/')
-  DB_PASSWORD="$(echo $userpass | grep : | cut -d: -f2)"
-  if [ -n "$DB_PASSWORD" ]; then
-    DB_USER=$(echo $userpass | grep : | cut -d: -f1)
-  else
-    DB_USER=$userpass
-  fi
-
-  # extract the host -- updated
-  hostport=`echo $url | sed -e s,$userpass@,,g | cut -d/ -f1`
-  port=`echo $hostport | grep : | cut -d: -f2`
-  if [ -n "$port" ]; then
-      DB_HOST=`echo $hostport | grep : | cut -d: -f1`
-      DB_PORT=$port
-  else
-      DB_HOST=$hostport
-  fi
-
-  DB_NAME="$(echo $url | grep / | cut -d/ -f2-)"
-fi
-
-# Write the password with MD5 encryption, to avoid printing it during startup.
-# Notice that `docker inspect` will show unencrypted env variables.
-if [ -n "$DB_USER" -a -n "$DB_PASSWORD" ] && ! grep -q "^\"$DB_USER\"" ${PG_CONFIG_DIR}/userlist.txt; then
-  if [ "$AUTH_TYPE" != "plain" ]; then
-     pass="md5$(echo -n "$DB_PASSWORD$DB_USER" | md5sum | cut -f 1 -d ' ')"
-  else
-     pass="$DB_PASSWORD"
-  fi
-  echo "\"$DB_USER\" \"$pass\"" >> ${PG_CONFIG_DIR}/userlist.txt
-  echo "Wrote authentication credentials to ${PG_CONFIG_DIR}/userlist.txt"
-fi
+PG_USER=postgres
 
 if [ ! -f ${PG_CONFIG_DIR}/pgbouncer.ini ]; then
-  echo "Create pgbouncer config in ${PG_CONFIG_DIR}"
+    echo "create pgbouncer config in ${PG_CONFIG_DIR}"
+    mkdir -p ${PG_CONFIG_DIR}
+    
+    if [ -f "${DB_PASSWORD_FILE}" ] ; then
+        DB_PASSWORD="$(cat "${DB_PASSWORD_FILE}")"
+    fi
 
-# Config file is in “ini” format. Section names are between “[” and “]”.
-# Lines starting with “;” or “#” are taken as comments and ignored.
-# The characters “;” and “#” are not recognized when they appear later in the line.
-  printf "\
-################## Auto generated ##################
+echo "# pgbouncer.ini
+# Description
+# Config file is in 'ini' format. Section names are between '[' and ']'.
+# Lines starting with ';' or '#' are taken as comments and ignored.
+# The characters ';' and '#' are not recognized when they appear later in the line.
 [databases]
-${DB_NAME:-*} = host=${DB_HOST:?"Setup pgbouncer config error! You must set DB_HOST env"} \
-port=${DB_PORT:-5432} user=${DB_USER:-postgres}
-${CLIENT_ENCODING:+client_encoding = ${CLIENT_ENCODING}\n}\
-
+* = host=${DB_HOST:?'Setup pgbouncer config error! You must set DB_HOST env'} port=${DB_PORT:-5432} user=${DB_USER:-postgres} ${DB_PASSWORD:+password=${DB_PASSWORD}}
 [pgbouncer]
+${LOGFILE:+logfile = ${LOGFILE}}
+${PIDFILE:+pidfile = ${PIDFILE}}
 listen_addr = ${LISTEN_ADDR:-0.0.0.0}
-listen_port = ${LISTEN_PORT:-5432}
-unix_socket_dir =
-user = postgres
-auth_file = ${AUTH_FILE:-$PG_CONFIG_DIR/userlist.txt}
-${AUTH_HBA_FILE:+auth_hba_file = ${AUTH_HBA_FILE}\n}\
-auth_type = ${AUTH_TYPE:-md5}
-${AUTH_USER:+auth_user = ${AUTH_USER}\n}\
-${AUTH_QUERY:+auth_query = ${AUTH_QUERY}\n}\
-${POOL_MODE:+pool_mode = ${POOL_MODE}\n}\
-${MAX_CLIENT_CONN:+max_client_conn = ${MAX_CLIENT_CONN}\n}\
-${DEFAULT_POOL_SIZE:+default_pool_size = ${DEFAULT_POOL_SIZE}\n}\
-${MIN_POOL_SIZE:+min_pool_size = ${MIN_POOL_SIZE}\n}\
-${RESERVE_POOL_SIZE:+reserve_pool_size = ${RESERVE_POOL_SIZE}\n}\
-${RESERVE_POOL_TIMEOUT:+reserve_pool_timeout = ${RESERVE_POOL_TIMEOUT}\n}\
-${MAX_DB_CONNECTIONS:+max_db_connections = ${MAX_DB_CONNECTIONS}\n}\
-${MAX_USER_CONNECTIONS:+max_user_connections = ${MAX_USER_CONNECTIONS}\n}\
-${SERVER_ROUND_ROBIN:+server_round_robin = ${SERVER_ROUND_ROBIN}\n}\
+${LISTEN_PORT:+listen_port = ${LISTEN_PORT}}
+${UNIX_SOCKET_DIR:+unix_socket_dir = ${UNIX_SOCKET_DIR}}
+${UNIX_SOCKET_MODE:+unix_socket_mode = ${UNIX_SOCKET_MODE}}
+${UNIX_SOCKET_GROUP:+unix_socket_group = ${UNIX_SOCKET_GROUP}}
+${USER:+user = ${USER}}
+${AUTH_FILE:+auth_file = ${AUTH_FILE}}
+${AUTH_HBA_FILE:+auth_hba_file = ${AUTH_HBA_FILE}}
+auth_type = ${AUTH_TYPE:-any}
+${AUTH_QUERY:+auth_query = ${AUTH_QUERY}}
+${POOL_MODE:+pool_mode = ${POOL_MODE}}
+${MAX_CLIENT_CONN:+max_client_conn = ${MAX_CLIENT_CONN}}
+${DEFAULT_POOL_SIZE:+default_pool_size = ${DEFAULT_POOL_SIZE}}
+${MIN_POOL_SIZE:+min_pool_size = ${MIN_POOL_SIZE}}
+${RESERVE_POOL_SIZE:+reserve_pool_size = ${RESERVE_POOL_SIZE}}
+${RESERVE_POOL_TIMEOUT:+reserve_pool_timeout = ${RESERVE_POOL_TIMEOUT}}
+${MAX_DB_CONNECTIONS:+max_db_connections = ${MAX_DB_CONNECTIONS}}
+${MAX_USER_CONNECTIONS:+max_user_connections = ${MAX_USER_CONNECTIONS}}
+${SERVER_ROUND_ROBIN:+server_round_robin = ${SERVER_ROUND_ROBIN}}
 ignore_startup_parameters = ${IGNORE_STARTUP_PARAMETERS:-extra_float_digits}
-${DISABLE_PQEXEC:+disable_pqexec = ${DISABLE_PQEXEC}\n}\
-${APPLICATION_NAME_ADD_HOST:+application_name_add_host = ${APPLICATION_NAME_ADD_HOST}\n}\
-
-# Log settings
-${LOG_CONNECTIONS:+log_connections = ${LOG_CONNECTIONS}\n}\
-${LOG_DISCONNECTIONS:+log_disconnections = ${LOG_DISCONNECTIONS}\n}\
-${LOG_POOLER_ERRORS:+log_pooler_errors = ${LOG_POOLER_ERRORS}\n}\
-${LOG_STATS:+log_stats = ${LOG_STATS}\n}\
-${STATS_PERIOD:+stats_period = ${STATS_PERIOD}\n}\
-${VERBOSE:+verbose = ${VERBOSE}\n}\
+${DISABLE_PQEXEC:+disable_pqexec = ${DISABLE_PQEXEC}}
+${APPLICATION_NAME_ADD_HOST:+application_name_add_host = ${APPLICATION_NAME_ADD_HOST}}
+${CONFFILE:+conffile = ${CONFFILE}}
+${JOB_NAME:+job_name = ${JOB_NAME}}
+${SYSLOG:+syslog = ${SYSLOG}}
+${SYSLOG_IDENT:+syslog_ident = ${SYSLOG_IDENT}}
+${SYSLOG_FACILITY:+syslog_facility = ${SYSLOG_FACILITY}}
+${LOG_CONNECTIONS:+log_connections = ${LOG_CONNECTIONS}}
+${LOG_DISCONNECTIONS:+log_disconnections = ${LOG_DISCONNECTIONS}}
+${LOG_POOLER_ERRORS:+log_pooler_errors = ${LOG_POOLER_ERRORS}}
+${STATS_PERIOD:+stats_period = ${STATS_PERIOD}}
+${VERBOSE:+verbose = ${VERBOSE}}
 admin_users = ${ADMIN_USERS:-postgres}
-${STATS_USERS:+stats_users = ${STATS_USERS}\n}\
-
-# Connection sanity checks, timeouts
-${SERVER_RESET_QUERY:+server_reset_query = ${SERVER_RESET_QUERY}\n}\
-${SERVER_RESET_QUERY_ALWAYS:+server_reset_query_always = ${SERVER_RESET_QUERY_ALWAYS}\n}\
-${SERVER_CHECK_DELAY:+server_check_delay = ${SERVER_CHECK_DELAY}\n}\
-${SERVER_CHECK_QUERY:+server_check_query = ${SERVER_CHECK_QUERY}\n}\
-${SERVER_LIFETIME:+server_lifetime = ${SERVER_LIFETIME}\n}\
-${SERVER_IDLE_TIMEOUT:+server_idle_timeout = ${SERVER_IDLE_TIMEOUT}\n}\
-${SERVER_CONNECT_TIMEOUT:+server_connect_timeout = ${SERVER_CONNECT_TIMEOUT}\n}\
-${SERVER_LOGIN_RETRY:+server_login_retry = ${SERVER_LOGIN_RETRY}\n}\
-${CLIENT_LOGIN_TIMEOUT:+client_login_timeout = ${CLIENT_LOGIN_TIMEOUT}\n}\
-${AUTODB_IDLE_TIMEOUT:+autodb_idle_timeout = ${AUTODB_IDLE_TIMEOUT}\n}\
-${DNS_MAX_TTL:+dns_max_ttl = ${DNS_MAX_TTL}\n}\
-${DNS_NXDOMAIN_TTL:+dns_nxdomain_ttl = ${DNS_NXDOMAIN_TTL}\n}\
-
-# TLS settings
-${CLIENT_TLS_SSLMODE:+client_tls_sslmode = ${CLIENT_TLS_SSLMODE}\n}\
-${CLIENT_TLS_KEY_FILE:+client_tls_key_file = ${CLIENT_TLS_KEY_FILE}\n}\
-${CLIENT_TLS_CERT_FILE:+client_tls_cert_file = ${CLIENT_TLS_CERT_FILE}\n}\
-${CLIENT_TLS_CA_FILE:+client_tls_ca_file = ${CLIENT_TLS_CA_FILE}\n}\
-${CLIENT_TLS_PROTOCOLS:+client_tls_protocols = ${CLIENT_TLS_PROTOCOLS}\n}\
-${CLIENT_TLS_CIPHERS:+client_tls_ciphers = ${CLIENT_TLS_CIPHERS}\n}\
-${CLIENT_TLS_ECDHCURVE:+client_tls_ecdhcurve = ${CLIENT_TLS_ECDHCURVE}\n}\
-${CLIENT_TLS_DHEPARAMS:+client_tls_dheparams = ${CLIENT_TLS_DHEPARAMS}\n}\
-${SERVER_TLS_SSLMODE:+server_tls_sslmode = ${SERVER_TLS_SSLMODE}\n}\
-${SERVER_TLS_CA_FILE:+server_tls_ca_file = ${SERVER_TLS_CA_FILE}\n}\
-${SERVER_TLS_KEY_FILE:+server_tls_key_file = ${SERVER_TLS_KEY_FILE}\n}\
-${SERVER_TLS_CERT_FILE:+server_tls_cert_file = ${SERVER_TLS_CERT_FILE}\n}\
-${SERVER_TLS_PROTOCOLS:+server_tls_protocols = ${SERVER_TLS_PROTOCOLS}\n}\
-${SERVER_TLS_CIPHERS:+server_tls_ciphers = ${SERVER_TLS_CIPHERS}\n}\
-
-# Dangerous timeouts
-${QUERY_TIMEOUT:+query_timeout = ${QUERY_TIMEOUT}\n}\
-${QUERY_WAIT_TIMEOUT:+query_wait_timeout = ${QUERY_WAIT_TIMEOUT}\n}\
-${CLIENT_IDLE_TIMEOUT:+client_idle_timeout = ${CLIENT_IDLE_TIMEOUT}\n}\
-${IDLE_TRANSACTION_TIMEOUT:+idle_transaction_timeout = ${IDLE_TRANSACTION_TIMEOUT}\n}\
-${PKT_BUF:+pkt_buf = ${PKT_BUF}\n}\
-${MAX_PACKET_SIZE:+max_packet_size = ${MAX_PACKET_SIZE}\n}\
-${LISTEN_BACKLOG:+listen_backlog = ${LISTEN_BACKLOG}\n}\
-${SBUF_LOOPCNT:+sbuf_loopcnt = ${SBUF_LOOPCNT}\n}\
-${SUSPEND_TIMEOUT:+suspend_timeout = ${SUSPEND_TIMEOUT}\n}\
-${TCP_DEFER_ACCEPT:+tcp_defer_accept = ${TCP_DEFER_ACCEPT}\n}\
-${TCP_KEEPALIVE:+tcp_keepalive = ${TCP_KEEPALIVE}\n}\
-${TCP_KEEPCNT:+tcp_keepcnt = ${TCP_KEEPCNT}\n}\
-${TCP_KEEPIDLE:+tcp_keepidle = ${TCP_KEEPIDLE}\n}\
-${TCP_KEEPINTVL:+tcp_keepintvl = ${TCP_KEEPINTVL}\n}\
-################## end file ##################
-" > ${PG_CONFIG_DIR}/pgbouncer.ini
-cat ${PG_CONFIG_DIR}/pgbouncer.ini
-echo "Starting $*..."
+${STATS_USERS:+stats_users = ${STATS_USERS}}
+${SERVER_RESET_QUERY:+server_reset_query = ${SERVER_RESET_QUERY}}
+${SERVER_RESET_QUERY_ALWAYS:+server_reset_query_always = ${SERVER_RESET_QUERY_ALWAYS}}
+${SERVER_CHECK_DELAY:+server_check_delay = ${SERVER_CHECK_DELAY}}
+${SERVER_CHECK_QUERY:+server_check_query = ${SERVER_CHECK_QUERY}}
+${SERVER_LIFETIME:+server_lifetime = ${SERVER_LIFETIME}}
+${SERVER_IDLE_TIMEOUT:+server_idle_timeout = ${SERVER_IDLE_TIMEOUT}}
+${SERVER_CONNECT_TIMEOUT:+server_connect_timeout = ${SERVER_CONNECT_TIMEOUT}}
+${SERVER_LOGIN_RETRY:+server_login_retry = ${SERVER_LOGIN_RETRY}}
+${CLIENT_LOGIN_TIMEOUT:+client_login_timeout = ${CLIENT_LOGIN_TIMEOUT}}
+${AUTODB_IDLE_TIMEOUT:+autodb_idle_timeout = ${AUTODB_IDLE_TIMEOUT}}
+${DNS_MAX_TTL:+dns_max_ttl = ${DNS_MAX_TTL}}
+${DNS_NXDOMAIN_TTL:+dns_nxdomain_ttl = ${DNS_NXDOMAIN_TTL}}
+${CLIENT_TLS_SSLMODE:+client_tls_sslmode = ${CLIENT_TLS_SSLMODE}}
+${CLIENT_TLS_KEY_FILE:+client_tls_key_file = ${CLIENT_TLS_KEY_FILE}}
+${CLIENT_TLS_CERT_FILE:+client_tls_cert_file = ${CLIENT_TLS_CERT_FILE}}
+${CLIENT_TLS_CA_FILE:+client_tls_ca_file = ${CLIENT_TLS_CA_FILE}}
+${CLIENT_TLS_PROTOCOLS:+client_tls_protocols = ${CLIENT_TLS_PROTOCOLS}}
+${CLIENT_TLS_CIPHERS:+client_tls_ciphers = ${CLIENT_TLS_CIPHERS}}
+${CLIENT_TLS_ECDHCURVE:+client_tls_ecdhcurve = ${CLIENT_TLS_ECDHCURVE}}
+${CLIENT_TLS_DHEPARAMS:+client_tls_dheparams = ${CLIENT_TLS_DHEPARAMS}}
+${SERVER_TLS_SSLMODE:+server_tls_sslmode = ${SERVER_TLS_SSLMODE}}
+${SERVER_TLS_CA_FILE:+server_tls_ca_file = ${SERVER_TLS_CA_FILE}}
+${SERVER_TLS_KEY_FILE:+server_tls_key_file = ${SERVER_TLS_KEY_FILE}}
+${SERVER_TLS_CERT_FILE:+server_tls_cert_file = ${SERVER_TLS_CERT_FILE}}
+${SERVER_TLS_PROTOCOLS:+server_tls_protocols = ${SERVER_TLS_PROTOCOLS}}
+${SERVER_TLS_CIPHERS:+server_tls_ciphers = ${SERVER_TLS_CIPHERS}}
+${QUERY_TIMEOUT:+query_timeout = ${QUERY_TIMEOUT}}
+${QUERY_WAIT_TIMEOUT:+query_wait_timeout = ${QUERY_WAIT_TIMEOUT}}
+${CLIENT_IDLE_TIMEOUT:+client_idle_timeout = ${CLIENT_IDLE_TIMEOUT}}
+${IDLE_TRANSACTION_TIMEOUT:+idle_transaction_timeout = ${IDLE_TRANSACTION_TIMEOUT}}
+${PKT_BUF:+pkt_buf = ${PKT_BUF}}
+${MAX_PACKET_SIZE:+max_packet_size = ${MAX_PACKET_SIZE}}
+${LISTEN_BACKLOG:+listen_backlog = ${LISTEN_BACKLOG}}
+${SBUF_LOOPCNT:+sbuf_loopcnt = ${SBUF_LOOPCNT}}
+${SUSPEND_TIMEOUT:+suspend_timeout = ${SUSPEND_TIMEOUT}}
+${TCP_DEFER_ACCEPT:+tcp_defer_accept = ${TCP_DEFER_ACCEPT}}
+${TCP_KEEPALIVE:+tcp_keepalive = ${TCP_KEEPALIVE}}
+${TCP_KEEPCNT:+tcp_keepcnt = ${TCP_KEEPCNT}}
+${TCP_KEEPIDLE:+tcp_keepidle = ${TCP_KEEPIDLE}}
+${TCP_KEEPINTVL:+tcp_keepintvl = ${TCP_KEEPINTVL}}" \
+| sed -e '/^$/d' \
+> ${PG_CONFIG_DIR}/pgbouncer.ini
 fi
 
-exec "$@"
+adduser ${PG_USER}
+mkdir -p "${PG_LOG}"
+chmod -R 755 "${PG_LOG}"
+chown -R ${PG_USER}:${PG_USER} "${PG_LOG}"
+
+if [ -z "$QUIET" ]; then
+    cat "${PG_CONFIG_DIR}/pgbouncer.ini"
+fi
+
+echo "Starting pgbouncer..."
+exec pgbouncer ${QUIET:+-q} -u ${PG_USER} "${PG_CONFIG_DIR}/pgbouncer.ini"
